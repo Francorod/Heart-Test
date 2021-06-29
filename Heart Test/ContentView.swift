8945f628ce086583c1262f86285e0c8482aefc81
Franco Rod <81878824+Francorod@users.noreply.github.com>
2021-06-29 16:14:58 -0700

    Initial Commit

---
+++ Heart Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Heart Test
+//
+//  Created by Franco Rodrigues on 6/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
